90e538c895114ac53e410bd9b35e46e5883369be
Jason R. Clark <jasonclark42@gmail.com>
2017-04-03 20:21:43 -0700

    Whitespace!

--- base/app.bat
+++ base/app.bat
@@ -8,7 +8,7 @@ if %ERRORLEVEL%==9009 (
     md "%USERPROFILE%\.shoes"
 
     helpers\wget --no-check-certificate https://github.com/jasonrclark/windows-app-templates/releases/download/jdk1.8.0_121/jdk.zip -O %USERPROFILE%\.shoes\jdk.zip
-  	helpers\unzip %USERPROFILE%\.shoes\jdk.zip -d %USERPROFILE%\.shoes
+    helpers\unzip %USERPROFILE%\.shoes\jdk.zip -d %USERPROFILE%\.shoes
   )
 
   set PATH=%USERPROFILE%\.shoes\jdk\bin;%PATH%
