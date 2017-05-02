94d73d8c5e0bacdf49368c7867b47f9b9f335084
Jason R. Clark <jason@jasonrclark.net>
2017-05-01 21:43:27 -0700

    Apply directory to helpers too

--- base/app.bat
+++ base/app.bat
@@ -7,8 +7,8 @@ if %ERRORLEVEL%==9009 (
     echo No system Java found. Downloading and installing a Shoes copy.
     md "%USERPROFILE%\.shoes"
 
-    helpers\wget --no-check-certificate https://github.com/jasonrclark/windows-app-templates/releases/download/jdk1.8.0_121/jdk.zip -O %USERPROFILE%\.shoes\jdk.zip
-    helpers\unzip %USERPROFILE%\.shoes\jdk.zip -d %USERPROFILE%\.shoes
+    %~dp0\helpers\wget --no-check-certificate https://github.com/jasonrclark/windows-app-templates/releases/download/jdk1.8.0_121/jdk.zip -O %USERPROFILE%\.shoes\jdk.zip
+    %~dp0\helpers\unzip %USERPROFILE%\.shoes\jdk.zip -d %USERPROFILE%\.shoes
   )
 
   set PATH=%USERPROFILE%\.shoes\jdk\bin;%PATH%
